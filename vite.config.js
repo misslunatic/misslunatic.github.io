5e318b30f75551a1df62985613600b8267cbf9b1
Seven Of Aces <sevenofaces42@gmail.com>
2024-09-18 10:33:57 -0700

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/blog/",
-  plugins: [],
+  plugins: [
+    vue()
+  ],
   resolve: {
     alias: [
 
