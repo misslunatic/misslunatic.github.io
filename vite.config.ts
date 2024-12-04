ea2a2db8090ae4e3e40514308f1f04425a9fd447
Seven Of Aces <sevenofaces42@gmail.com>
2024-12-03 19:56:29 -0800

    :3

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+    
+        main: resolve(__dirname, 'index.html'),
+        
+        blog: resolve(__dirname, 'src/pages/Blog.vue'),
+        
+      },
+    },
+  }, 
   plugins: [
     vue()
   ],
